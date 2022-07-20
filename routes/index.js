0c9e5ec8feeeae8f5b25ce4840fb1c8660ddfc0a
hilmihs <hilmisobandi@gmail.com>
2022-07-20 07:16:43 +0700

    db and interface done

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment')
+module.exports = function (db) {
+  /* GET home page. */
+  router.get('/', function (req, res, next) {
+    db.query('select * from simple_inventory', (err, rows) => {
+      if (err) console.log(err)
+      console.log(rows.rows[0])
+      res.render('sidebars', { rows: rows.rows, moment });
+    })
+    
+  });
+
+  return router;
+}
