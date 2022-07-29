68bd390eafaa7ef1eec1a281e76a5890b26ac837
hilmihs <hilmisobandi@gmail.com>
2022-07-29 15:17:28 +0700

    pasang heroku

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 var indexRouter = require('./routes/index')(pool);
